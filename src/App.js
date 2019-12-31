dc6867ec3da4a75854bd7f5507438003ebad6657
Adedolapo Adaranijo <dolapo098@yahoo.com>
2019-12-31 10:02:43 +0100

    my portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CustomRouter from './components/CustomRouter';
+// import LayOut from "./components/LayOut"
+
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <LayOut/> */}
+      <CustomRouter></CustomRouter>
+    
     </div>
   );
 }
